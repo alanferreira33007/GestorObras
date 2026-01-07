8188aed355d64e75cc595282196052dff60c3f83
alanferreira33007 <alanferreira.33007@gmail.com>
2026-01-07 14:28:08 -0300

    Create sheets.py

---
+++ core/sheets.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+@st.cache_resource
+def get_db():
+    creds_json = json.loads(st.secrets["gcp_service_account"]["json_content"], strict=False)
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    client = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope))
+    return client.open("GestorObras_DB")
