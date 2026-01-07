cba6a2e012df1ca34d4f7678c4d01cc519b99896
alanferreira33007 <alanferreira.33007@gmail.com>
2026-01-07 14:24:42 -0300

    Add default constants for obras and categories

---
+++ core/constants.py
@@ -0,0 +1,5 @@
+OBRAS_COLS = ["ID", "Cliente", "Endereço", "Status", "Valor Total", "Data Início", "Prazo"]
+FIN_COLS   = ["Data", "Tipo", "Categoria", "Descrição", "Valor", "Obra Vinculada"]
+
+CATEGORIAS_PADRAO = ["Geral", "Material", "Mão de Obra", "Serviços", "Impostos", "Outros"]
+STATUS_PADRAO = ["Planejamento", "Construção", "Finalizada"]
