d4ae42776a019fbb01ff3afbccd99214536b2082
alanferreira33007 <alanferreira.33007@gmail.com>
2026-01-07 23:06:56 -0300

    Delete core directory

--- core/constants.py
+++
@@ -1,5 +0,0 @@
-OBRAS_COLS = ["ID", "Cliente", "Endereço", "Status", "Valor Total", "Data Início", "Prazo"]
-FIN_COLS   = ["Data", "Tipo", "Categoria", "Descrição", "Valor", "Obra Vinculada"]
-
-CATEGORIAS_PADRAO = ["Geral", "Material", "Mão de Obra", "Serviços", "Impostos", "Outros"]
-STATUS_PADRAO = ["Planejamento", "Construção", "Finalizada"]
